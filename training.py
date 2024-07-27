8cfe796b996575fee816b33ac55b518b84cd7bb9
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-07-28 00:51:59 +0700

    Update training.py

--- training.py
+++ training.py
@@ -2,13 +2,13 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/training')
+os.chdir('/content/faceswabgit')
 
-full_local_path = "/content/training"
+full_local_path = "/content/faceswabgit"
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
-repo.index.commit("user_fakes")
+repo.index.commit(tgl)
 
 origin = repo.remote(name="origin")
 origin.push()
